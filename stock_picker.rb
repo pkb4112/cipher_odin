0274621c5a5fba04a38080709a25ec117a833b7b
Peter Brandon <peterkbrandon@gmail.com>
2017-08-15 15:42:22 -0400

    stock picker improved

--- stock_picker.rb
+++ stock_picker.rb
@@ -1,14 +1,32 @@
+stock_data = [15,3,6,7,9,4,19,2,1,16,20,19,25,1]
+
 def stock_picker(days)
-	max = days.max
-	min = days.min 
 
-	max_day = days.index(max)
-	min_day = days.index(min)
-	  
-	best_days = [max_day+1,min_day+1]
+	profit = [0,1,2]
+	profit_test = 0
+	min_day = 9999999999
+	
 
-	return best_days
+	for i in (0..days.size-1) do
+		
+	   for j in (0..days.size-1) do
+	   	
 
-end
+	   	profit_test = days[j]-days[i]
 
-puts stock_picker([15,3,6,7,9,4,19,2])
+		   	if profit_test > profit[0] && i<j  # must buy before you can sell
+                buy = i
+                sell = j
+		   		profit = [profit_test, buy, sell]
+
+		   	end # if statement
+
+	   end  # for j
+
+	end # for i
+
+	return profit
+
+end # method
+
+print "Profit: #{stock_picker(stock_data)[0]}, Buy on Day: #{stock_picker(stock_data)[1]} , Sell on Day: #{stock_picker(stock_data)[2]} \n"
